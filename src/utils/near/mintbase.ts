be6c1c0d9717d108ea71c44306212e064c70a0f9
Sérgio Rebelo <chinoman10@msn.com>
2023-11-09 02:30:39 +0000

    Mintbase public GraphQL query

---
+++ src/utils/near/mintbase.ts
@@ -0,0 +1,42 @@
+import { MintbaseNFT } from '../../types';
+import { handleJsonRequest } from '..';
+const MINTBASE_GRAPH_URL = "https://graph.mintbase.xyz/testnet";
+
+export async function getNFTsBalances(account: string): Promise<MintbaseNFT[]> {
+    try {
+        const graphQL = JSON.stringify({
+            query: `
+                query QueryBasicNFTOwnership($ownerId: String!) {
+                    mb_views_nft_tokens(
+                        where: {
+                            owner: {_eq: $ownerId}, 
+                            _and: {burned_timestamp: {_is_null: true}}
+                        }
+                        limit: 30
+                        order_by: {last_transfer_timestamp: desc}
+                    ) {
+                        nft_contract_id
+                        title
+                        description
+                        media
+                    }
+                }`,
+            variables: {
+                "ownerId": account
+            }
+        });
+
+		const res = await fetch(`${MINTBASE_GRAPH_URL}`, {
+			method: 'POST',
+			headers: {
+				'mb-api-key': 'anon',
+			},
+            body: graphQL
+		}).then((r) => handleJsonRequest(r, 1));
+
+        return res.data.mb_views_nft_tokens;
+	} catch (error) {
+		console.error(error);
+		return [];
+	}
+}
